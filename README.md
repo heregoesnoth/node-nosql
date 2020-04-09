06cd1416aeb0127735390ad74ff86ea2bec16c66
Lucas Gomes <lucasgms16@gmail.com>
2020-04-09 01:38:29 -0300

    NodeJS with MongoDB and Redis

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# node-nosql
+Projeto criado para demonstrar como usar MongoDB e Redis com NodeJS
---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const personRoute = require('./routes/personRoute');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json())
+
+app.use('/', index);
+app.use('/person', personRoute);
+
+module.exports = app;
